a1c95623a6099233cc9d2fd1eb10b720340bedb5
Lekshmi-lab <52638090+Lekshmi-lab@users.noreply.github.com>
2019-07-07 20:30:29 -0500

    New python notebook added

---
+++ GitHub project.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# Main use for the Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+print ('Hello Capstone Project Course!')
+
